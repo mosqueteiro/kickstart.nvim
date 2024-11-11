3c4e625f94aa351fa383775cf4c1411e9ea6d14a
Mosqueteiro <nat3.th3.gr3at@gmail.com>
2024-11-10 22:40:46 -0700

    add notes and debug functions for Snacks
    Add notes about needing lazygit installed separately to be able to use
    the lazygit functionality. Also added functionality for the debugger (as
    explained in the README). Finally removed some commented out lines that
    are no longer relevant

--- init.lua
+++ init.lua
@@ -858,11 +858,10 @@ require('lazy').setup({
 
   {
     'folke/snacks.nvim',
+    -- Notes:
+    -- lazygit, the cli program, actually needs to be installed separately
     priority = 1000,
     lazy = false,
-    -- dependencies = {
-    --   'kdheepak/lazygit.nvim',
-    -- },
     -- Snacks = require 'snacks',
     ---@type snacks.Config
     opts = {
@@ -986,6 +985,32 @@ require('lazy').setup({
         end,
       },
     },
+    init = function()
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'VeryLazy',
+        callback = function()
+          -- Setup some globals for debugging (lazy-loaded)
+          _G.dd = function(...)
+            Snacks.debug.inspect(...)
+          end
+          _G.bt = function()
+            Snacks.debug.backtrace()
+          end
+          vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+          -- Create some toggle mappings
+          Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
+          Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
+          Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>uL'
+          Snacks.toggle.diagnostics():map '<leader>ud'
+          Snacks.toggle.line_number():map '<leader>ul'
+          Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map '<leader>uc'
+          Snacks.toggle.treesitter():map '<leader>uT'
+          Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
+          Snacks.toggle.inlay_hints():map '<leader>uh'
+        end,
+      })
+    end,
   },
 
   { -- Collection of various small independent plugins/modules
