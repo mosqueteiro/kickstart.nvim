f1d339fa32cf42a22483bad7a980798b60485ebb
Mosqueteiro <nat3.th3.gr3at@gmail.com>
2024-11-23 15:00:40 -0700

    add LazyClip
    LazyClip is a clipboard manager for NeoVim

--- init.lua
+++ init.lua
@@ -1086,6 +1086,13 @@ require('lazy').setup({
 
   { 'tpope/vim-obsession' },
 
+  {
+    'atiladefreitas/lazyclip',
+    keys = {
+      { '<leader>sc', ":lua require('lazyclip').show_clipboard()<CR>", desc = '[S]earch [C]lipboard manager' },
+    },
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
