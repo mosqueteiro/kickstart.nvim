a164268394a277c6252692b5b53ce438d3f64f8e
Mosqueteiro <nat3.th3.gr3at@gmail.com>
2024-11-23 14:59:33 -0700

    add key group g
    Adds more info for the Snacks commands that start with `leader-g`.
    Mainly LazyGit

--- init.lua
+++ init.lua
@@ -317,6 +317,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
+        { '<leader>g', group = 'Lazy[G]it' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
     },
