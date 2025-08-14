7cfceb2e512707fe21c845453057eea8b3c78b8f
Mosqueteiro <nat3.th3.gr3at@gmail.com>
2025-08-13 23:30:31 -0600

    add dbee database client

---
+++ lua/custom/plugins/dbee.lua
@@ -0,0 +1,17 @@
+-- dbee database client for NeoVim
+
+return {
+  'kndndrj/nvim-dbee',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+  },
+  build = function()
+    -- Install tries to automatically detect the install method
+    -- If it fails, try calling it with one of these parameters:
+    --   "curl", "wget", "bitsadmin", "go"
+    require('dbee').install 'go'
+  end,
+  config = function()
+    require('dbee').setup {}
+  end,
+}
