2e7b4dd861a2014e19f9429d924a3dd118c5716e
Nathan James <njames@entelligent.com>
2025-08-15 18:08:45 -0600

    add dadbod database client

--- init.lua
+++ init.lua
@@ -1230,6 +1230,8 @@ require('lazy').setup({
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
   require 'custom.plugins.dbee',
+
+  require 'plugins.dadbod',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
---
+++ lua/plugins/dadbod.lua
@@ -0,0 +1,19 @@
+-- Database functionality for NeoVim
+
+return {
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod', lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    -- DBUI configuration goes here
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
+}
