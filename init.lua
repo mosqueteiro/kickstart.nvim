0bd08a5bc11ede031ec7d90c2d68b5a3a908a7e2
James, Nathan <Nathan.James+cvsh@aetna.com>
2025-12-17 11:49:13 -0700

    [nvim-cmp] Add todo to keybind a cancel cmp

--- init.lua
+++ init.lua
@@ -816,6 +816,8 @@ require('lazy').setup({
           --  Generally you don't need this, because nvim-cmp will display
           --  completions whenever it has completion options available.
           ['<C-Space>'] = cmp.mapping.complete {},
+          -- TODO: add something to cancel cmp
+          -- ['<C-S-Space'] = cmp.mapping.cancel
 
           -- Think of <c-l> as moving to the right of your snippet expansion.
           --  So if you have a snippet that's like:
