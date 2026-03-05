fd8ff1b15a5655808b0c93dae17c6b358d6426a0
Mosqueteiro <nat3.th3.gr3at@gmail.com>
2026-03-04 22:58:24 -0700

    [notes] add completions for render-markdown

--- init.lua
+++ init.lua
@@ -1281,7 +1281,9 @@ require('lazy').setup({
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    opts = {
+      completions = { lsp = { enabled = true } },
+    },
   },
 
   {
